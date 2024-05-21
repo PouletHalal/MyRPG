8d23e4269c396fb1ab0c4d9c3768138209318c09
LixiosDelios <alexandre.guillaud@epitech.eu>
2024-05-21 21:59:46 +0200

    fix: invalide vect in spawn mob

--- src/mob/spawn_mob.c
+++ src/mob/spawn_mob.c
@@ -10,17 +10,14 @@
 #include "player.h"
 #include "rendering.h"
 
-static void check_spawn(entity_t *new, world_t *world, sfVector2f vect,
-    win_t *window)
+static void check_spawn(entity_t *new, world_t *world, win_t *window)
 {
     new->mask |= COMP_STAT | COMP_MOB;
     new->comp_mob.is_alive = true;
     new->comp_mob.does_rand_spawn = false;
     new->comp_stat.do_respawn = false;
-    printf("t\n");
-    if (!check_collision(new, world, vect, window))
+    if (!check_collision(new, world, (sfVector2f) {0, 0}, window))
         return;
-    printf("y\n");
     sfSprite_destroy(new->comp_render.sprite);
     sfTexture_destroy(new->comp_render.texture);
     new->mask = COMP_NONE;
@@ -44,5 +41,5 @@ void spawn_copy(entity_t *entity, world_t *world, double angle, win_t *window)
     new->comp_mob = entity->comp_mob;
     new->comp_stat = entity->comp_stat;
     new->entity = free;
-    return check_spawn(new, world, vect, window);
+    return check_spawn(new, world, window);
 }
