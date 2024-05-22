bea584297c1dbd146b69a823f290ee75de978e92
alexis <alexis.constantinopoulos@epitech.eu>
2024-05-22 14:20:49 +0200

    fix: coding style

--- src/inventory/items_gestion.c
+++ src/inventory/items_gestion.c
@@ -47,10 +47,9 @@ static bool item_higlighting(win_t *window, world_t *world,
     return false;
 }
 
-static bool drop_item(win_t *window, entity_t *player, entity_t *item, int i)
+static bool drop_item(entity_t *player, entity_t *item, sfVector2i mouse_pos,
+    int i)
 {
-    sfVector2i mouse_pos = sfMouse_getPositionRenderWindow(window->window);
-
     //if (player->comp_input.mouse_right_down == false)
     //    return false;
     if (item->comp_item.type_mask == 0)
@@ -95,18 +94,20 @@ bool use_item(win_t *window, entity_t *player, entity_t *item, int i)
 
 void item_events(win_t *window, world_t *world, entity_t *entity)
 {
+    sfVector2i mouse_pos = sfMouse_getPositionRenderWindow(window->window);
+
     if ((entity->mask & COMP_INVENTORY) != COMP_INVENTORY ||
         entity->comp_inventory.is_open == false)
         return;
     for (int i = 0; i < entity->comp_inventory.size; i++) {
         if (item_higlighting(window, world, entity, i))
             return;
-        if (drop_item(window, entity, &world->entity[entity->
-        comp_inventory.items[i].id_in_world], i))
+        if (drop_item(entity, &world->entity[
+            entity->comp_inventory.items[i].id_in_world], mouse_pos, i))
             return;
-        if (use_item(window, entity, &world->entity[entity->
-        comp_inventory.items[i].id_in_world], i))
-            return;
+        //if (use_item(window, entity, &world->entity[entity->
+        //comp_inventory.items[i].id_in_world], i))
+        //    return;
     }
 }
 
@@ -168,46 +169,75 @@ int get_slot_from_pos(entity_t *entity, sfVector2i mouse_pos)
     return -1;
 }
 
+static void pick_item(world_t *world, entity_t *entity,
+    sfVector2i mouse_pos, entity_t *mouse)
+{
+    comp_item_t *items = entity->comp_inventory.items;
+
+    for (int i = 0; i < entity->comp_inventory.size; i++) {
+        if ((entity->comp_inventory.items[i].type_mask != 0)
+        && (is_mouse_over(mouse_pos, &world->entity[items[i].id_in_world])
+        && world->mouse_left_pressed)) {
+            mouse->comp_mouse.item_picked = true;
+            mouse->comp_mouse.item_picked_i = i;
+            entity->comp_inventory.items[i].is_picked = true;
+            world->mouse_left_pressed = false;
+            world->entity[items[i].id_in_world].comp_position.position =
+            (sfVector2f){mouse_pos.x, mouse_pos.y};
+            return;
+        }
+    }
+}
+
+static void drag_item_inv(entity_t *entity, entity_t *mouse, int slot)
+{
+    comp_item_t temp = (comp_item_t) {0};
+
+    if (slot > 0) {
+        entity->comp_inventory.items[
+            mouse->comp_mouse.item_picked_i].is_picked = false;
+        temp = entity->comp_inventory.items[mouse->comp_mouse.item_picked_i];
+        entity->comp_inventory.items[mouse->comp_mouse.item_picked_i] =
+        entity->comp_inventory.items[slot];
+        entity->comp_inventory.items[slot] = temp;
+        mouse->comp_mouse.item_picked = false;
+        mouse->comp_mouse.item_picked_i = 0;
+    }
+}
+
+static void drop_item_inv(entity_t *entity, world_t *world,
+    sfVector2i mouse_pos, int slot)
+{
+    int mouse_id = find_comp(world, COMP_MOUSE);
+    entity_t *mouse = &world->entity[mouse_id];
+    comp_item_t *items = entity->comp_inventory.items;
+
+    if (slot == -1) {
+        mouse->comp_mouse.item_picked = false;
+        drop_item(entity, &world->entity[items[
+            mouse->comp_mouse.item_picked_i].id_in_world], mouse_pos,
+            mouse->comp_mouse.item_picked_i);
+        mouse->comp_mouse.item_picked_i = 0;
+    }
+}
+
 void manage_inv_slots(world_t *world, win_t *window, entity_t *entity)
 {
     int mouse_id = find_comp(world, COMP_MOUSE);
     entity_t *mouse = &world->entity[mouse_id];
     sfVector2i mouse_pos = sfMouse_getPositionRenderWindow(window->window);
     comp_item_t *items = entity->comp_inventory.items;
-    comp_item_t temp = (comp_item_t) {0};
     int slot = get_slot_from_pos(entity, mouse_pos);
 
     if (mouse->comp_mouse.item_picked) {
-        world->entity[items[mouse->comp_mouse.item_picked_i].id_in_world].comp_position.position = (sfVector2f){mouse_pos.x, mouse_pos.y};
+        world->entity[items[mouse->comp_mouse.item_picked_i].id_in_world]
+        .comp_position.position = (sfVector2f){mouse_pos.x, mouse_pos.y};
         if (world->mouse_left_pressed) {
             world->mouse_left_pressed = false;
-            if (slot > 0) {
-            entity->comp_inventory.items[mouse->comp_mouse.item_picked_i].is_picked = false;
-            temp = entity->comp_inventory.items[mouse->comp_mouse.item_picked_i];
-            entity->comp_inventory.items[mouse->comp_mouse.item_picked_i] = entity->comp_inventory.items[slot];
-            entity->comp_inventory.items[slot] = temp;
-
-            mouse->comp_mouse.item_picked = false;
-            mouse->comp_mouse.item_picked_i = 0;
-            }
-            if (slot == -1) {
-                mouse->comp_mouse.item_picked = false;
-                drop_item(window, entity, &world->entity[items[mouse->comp_mouse.item_picked_i].id_in_world], mouse->comp_mouse.item_picked_i);
-                mouse->comp_mouse.item_picked_i = 0;
-            }
+            drag_item_inv(entity, mouse, slot);
+            drop_item_inv(entity, world, mouse_pos, slot);
         }
         return;
     }
-    for (int i = 0; i < entity->comp_inventory.size; i++) {
-        if (entity->comp_inventory.items[i].type_mask != 0) {
-            if (is_mouse_over(mouse_pos, &world->entity[items[i].id_in_world]) && world->mouse_left_pressed) {
-                mouse->comp_mouse.item_picked = true;
-                mouse->comp_mouse.item_picked_i = i;
-                entity->comp_inventory.items[i].is_picked = true;
-                world->mouse_left_pressed = false;
-                world->entity[items[i].id_in_world].comp_position.position = (sfVector2f){mouse_pos.x, mouse_pos.y};
-                return;
-            }
-        }
-    }
+    pick_item(world, entity, mouse_pos, mouse);
 }
